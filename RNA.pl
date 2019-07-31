01726630eb5cab6bf85b92a2e57463f2c08bcda7
Govind Mohan <govi218mu@gmail.com>
2019-07-31 00:25:28 -0400

    Create RNA.pl

---
+++ RNA.pl
@@ -0,0 +1,27 @@
+ribo_nucleic_acid --> start_codon,codons,stop_codon.
+
+codons --> codon.
+codons --> codon, codons.
+
+start_codon --> a,u,g.
+
+stop_codon --> ochre.
+stop_codon --> amber.
+stop_codon --> opal.
+
+ochre --> u,a,a.
+amber --> u,a,g.
+opal --> u,g,a.
+
+codon --> nucleobase,nucleobase,nucleobase.
+
+nucleobase --> a.
+nucleobase --> c.
+nucleobase --> u.
+nucleobase --> g.
+
+
+a --> [a].
+c --> [c].
+u --> [u].
+g --> [g].
